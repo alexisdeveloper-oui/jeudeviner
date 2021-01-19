e757d5daf1737d91b53e8dcae11dc65f5748d361
alexisdeveloper-oui <n1bb469@gmail.com>
2021-01-19 15:02:01 -0500

    Add project files.

---
+++ jeudeviner/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace jeudeviner
+{
+    class Program
+    {
+        static private string Message(int aTrouver, int choisi)
+        {
+            if (aTrouver == choisi)
+            {
+                return "Bravo mon chou xxx";
+            }
+            else if (aTrouver < choisi)
+            {
+                return "Le nombre est trop grand...";
+            }
+            else if (aTrouver > choisi)
+            {
+                return "Le nombre est trop petit...";
+            }
+            else
+            {
+                return "Erreur...";
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+
+            int nombreCoupsUser = 0;
+            int nombreCoupsOrdi = 0;
+            int nombreChoisi;
+
+            int max;
+
+            Console.Write("Entrez la valeur maximale du nombre qui sera généré : ");
+
+            max = int.Parse(Console.ReadLine());         
+            Console.WriteLine("L'ordinateur a choisi son nombre, appuyez sur une touche pour commencer.");
+            int nombreATrouver = random.Next(0, max);
+            Console.WriteLine(nombreATrouver); //pour debug
+            Console.ReadKey();
+
+            Console.Clear();
+            //Loop pour faire devimner l'humain
+            do
+            {
+                Console.Write("Entrez un nombre : ");
+                nombreChoisi = int.Parse(Console.ReadLine());
+                Console.WriteLine(Message(nombreATrouver, nombreChoisi));
+                nombreCoupsUser++;
+            } while (nombreChoisi != nombreATrouver);
+
+            Console.WriteLine("Vous avez trouvé le chiffre en question en {0} coups", (object)nombreCoupsUser);
+    
+
+        }
+    }
+}
